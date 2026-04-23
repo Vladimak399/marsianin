1cb22b6ee1adc81d6976cb788387f498d6f8ef32
Vladimak399 <makin05@yandex.ru>
2026-04-23 13:46:28 +0200

    Fix menu state sync for location/category navigation

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,9 +1,9 @@
 import MenuPage from '@/components/menu/MenuPage';
 
 type MenuRoutePageProps = {
-  searchParams: { location?: string };
+  searchParams: { location?: string; category?: string };
 };
 
 export default function MenuRoutePage({ searchParams }: MenuRoutePageProps) {
-  return <MenuPage initialLocation={searchParams.location} />;
+  return <MenuPage initialLocation={searchParams.location} initialCategory={searchParams.category} />;
 }
