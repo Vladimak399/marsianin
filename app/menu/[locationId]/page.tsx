532e2719676881b01136c6b5262a2012e75e9e1c
Vladimak399 <makin05@yandex.ru>
2026-04-25 11:27:15 +0200

    Validate menu location route params

--- app/menu/[locationId]/page.tsx
+++ app/menu/[locationId]/page.tsx
@@ -1,9 +1,15 @@
+import { notFound } from 'next/navigation';
+
 import MenuPage from '@/components/menu/MenuPage';
+import { locations } from '@/data/locations';
 
 type MenuByLocationRoutePageProps = {
   params: Promise<{ locationId: string }>;
   searchParams: Promise<{ category?: string; source?: string; guestLat?: string; guestLng?: string }>;
 };
+
+
+const validLocationIds = new Set(locations.map((location) => location.id));
 
 const parseCoordinate = (value?: string) => {
   if (!value) return null;
@@ -14,6 +20,10 @@ const parseCoordinate = (value?: string) => {
 
 export default async function MenuByLocationRoutePage({ params, searchParams }: MenuByLocationRoutePageProps) {
   const { locationId } = await params;
+  if (!validLocationIds.has(locationId)) {
+    notFound();
+  }
+
   const query = await searchParams;
   const guestLat = parseCoordinate(query.guestLat);
   const guestLng = parseCoordinate(query.guestLng);
